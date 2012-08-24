fea986456cbad70105341ebd092374eaf62c255c
PyroPeter <pyropeter@pyropeter.eu>
2012-08-24 22:19:40 +0200

    leerzeichen durch tabs ersetzt

--- embassyweb/urls.py
+++ embassyweb/urls.py
@@ -5,13 +5,13 @@ from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'embassyweb.views.home', name='home'),
-    # url(r'^embassyweb/', include('embassyweb.foo.urls')),
+	# Examples:
+	# url(r'^$', 'embassyweb.views.home', name='home'),
+	# url(r'^embassyweb/', include('embassyweb.foo.urls')),
 
-    # Uncomment the admin/doc line below to enable admin documentation:
-    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+	# Uncomment the admin/doc line below to enable admin documentation:
+	# url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
-    # Uncomment the next line to enable the admin:
-    url(r'^admin/', include(admin.site.urls)),
+	# Uncomment the next line to enable the admin:
+	url(r'^admin/', include(admin.site.urls)),
 )
