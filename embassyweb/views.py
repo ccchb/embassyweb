ac98c34c437c2d7ffa5544e98767824a3efeb7de
PyroPeter <pyropeter@pyropeter.eu>
2012-09-10 11:50:37 +0200

    layout-verbesserungen

--- embassyweb/views.py
+++ embassyweb/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from blog.models import Post
 
 def index(request):
-	posts = Post.objects.order_by('-updated')
+	posts = Post.objects.order_by('-created')
 
 	return render(request, "index.html", {"posts": posts})
 
