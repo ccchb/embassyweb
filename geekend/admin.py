01d8105da5d2d3523d980c42df8a7a6fb7b0948a
Tristan Bruns <tristan+git@tristanbruns.de>
2016-05-07 00:41:12 +0200

    also show email

--- geekend/admin.py
+++ geekend/admin.py
@@ -3,7 +3,8 @@ from models import Attendant
 
 class AttendantAdmin(admin.ModelAdmin):
     list_display = (
-            "handle", "vegetarian", "needs_place_to_sleep", "comments")
+            "handle", "vegetarian", "needs_place_to_sleep", "email",
+            "comments")
 
 admin.site.register(Attendant, AttendantAdmin)
 
